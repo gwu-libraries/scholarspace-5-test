0e44ac865c61ad1aff7d152cc78213af1359f201
Alex Boyd <alexanderleeboyd@gmail.com>
2025-07-24 17:17:40 -0400

    update prod and dev environment configs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "nurax-pg_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "scholarspace-pg_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
 
