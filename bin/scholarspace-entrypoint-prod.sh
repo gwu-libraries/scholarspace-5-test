e9b1e236dc8f39e140a522e06fdc9e5dadf90de4
Alex Boyd <alexanderleeboyd@gmail.com>
2025-08-04 21:34:41 -0400

    fix prod docker setup

--- bin/scholarspace-entrypoint-prod.sh
+++ bin/scholarspace-entrypoint-prod.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-# If running the rails server then create or migrate existing database
-# if [ "${1}" == "./bin/rails" ] && [ "${2}" == "server" ]; then
-./bin/rails db:create db:migrate RAILS_ENV=production
-# fi
+if [ "${1}" == "./bin/rails" ] && [ "${2}" == "server" ]; then
+  ./bin/rails db:create db:migrate RAILS_ENV=production
+fi
 
 exec "${@}"
