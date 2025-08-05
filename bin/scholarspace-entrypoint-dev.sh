b698ca16902979f7654f0a42999049ba3d0c7995
Alex Boyd <alexanderleeboyd@gmail.com>
2025-08-04 20:21:22 -0400

    fix dev entrypoint

--- bin/scholarspace-entrypoint-dev.sh
+++ bin/scholarspace-entrypoint-dev.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-./bin/rails db:create db:migrate RAILS_ENV=development
+if [ "${1}" == "./bin/rails" ] && [ "${2}" == "server" ]; then
+  ./bin/rails db:create db:migrate RAILS_ENV=development
+fi
 
 exec "${@}"
