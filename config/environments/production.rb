2cefff1c67e931b339591ac9d4f63cb8ef06e11a
Alex Boyd <alexanderleeboyd@gmail.com>
2025-08-07 15:53:32 -0400

    allow fallback to asset pipeline

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,7 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
+  config.public_file_server.enabled = true
 
   # Compress JavaScripts and CSS.
   config.assets.configure do |env|
@@ -32,7 +32,7 @@ Rails.application.configure do
   end
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
