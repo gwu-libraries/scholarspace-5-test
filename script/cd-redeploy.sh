945553bbdb4c3f9e186530d3831fa205e3551022
Alex Boyd <alexanderleeboyd@gmail.com>
2025-08-07 11:44:50 -0400

    testing cd webhook

---
+++ script/cd-redeploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Change directory to the folder
+cd /opt/scholarspace/scholarspace-5-test/
+# Stash any local changes
+git stash
+# Checkout main branch if not already on it
+git checkout main
+# Pull any changes  
+git pull
+# Stop and remove containers
+
+cat > docker-compose.yml << EOF
+include:
+  - docker-compose-prod.yml
+EOF
+
+docker-compose stop
+echo "Restarting Docker containers"
+docker-compose up -d
