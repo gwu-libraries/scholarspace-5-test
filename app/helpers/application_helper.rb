75a32791cfb380de072085e736db7b057b6765fb
Alex Boyd <alexanderleeboyd@gmail.com>
2025-11-17 13:55:47 -0500

    add without_params helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+  # This is just used in stripping the "?locale=en" when setting the path to the 
+  # pdf for pdfjs in 'hyrax/academic_documents/show.html.erb' 
+  def without_params(input_str)
+    input_str.include?('?') ? input_str[0...input_str.index('?')] : input_str
+  end
 end
