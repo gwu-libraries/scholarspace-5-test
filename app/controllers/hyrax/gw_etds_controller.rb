df898de43e5fec4761471ec33fd03302d5f0bd7e
Dolsy Smith <dsmith@gwu.edu>
2025-11-18 14:19:05 +0000

    GwEtd code

---
+++ app/controllers/hyrax/gw_etds_controller.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module Hyrax
+  class GwEtdsController < ApplicationController
+    # Adds Hyrax behaviors to the controller.
+    include Hyrax::WorksControllerBehavior
+    include Hyrax::BreadcrumbsForWorks
+    self.curation_concern_type = ::GwEtd
+
+    # Use a Valkyrie aware form service to generate Valkyrie::ChangeSet style
+    # forms.
+    self.work_form_service = Hyrax::FormFactory.new
+  end
+end
