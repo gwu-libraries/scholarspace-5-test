e5f563e8716ab0b154344452d971f59bc570e80b
Dolsy Smith <dsmith@gwu.edu>
2025-12-01 20:41:57 +0000

    Added CSV parser config table

--- config/initializers/bulkrax.rb
+++ config/initializers/bulkrax.rb
@@ -11,7 +11,7 @@ Rails.application.config.to_prepare do
     # config.default_work_type = "MyWork"
 
     # Factory Class to use when generating and saving objects
-    config.object_factory = Bulkrax::ObjectFactory
+    config.object_factory = Bulkrax::ValkyrieObjectFactory
     # Use this for a Postgres-backed Valkyrized Hyrax
     # config.object_factory = Bulkrax::ValkyrieObjectFactory
 
@@ -34,6 +34,58 @@ Rails.application.config.to_prepare do
     # config.collection_field_mapping['Bulkrax::RdfEntry'] = 'http://opaquenamespace.org/ns/set'
 
     # Field mappings
+    config.field_mappings['Bulkrax::CsvParser'] = {
+       # Setting source_identifier: true makes bulkrax_identifier a mandatory field,
+       # so it MUST be present in the CSV row for EVERY item (regardless of type, so this includes FileSets as well)
+       #
+       # from Hyrax::BasicMetadata
+       'label' => { from: ['label'], split: true },
+       'relative_path' => { from: ['relative_path'], split: true },
+       'import_url' => { from: ['import_url'], split: '\|' },
+       'resource_type' => {from: ['resource_type'], split: true },
+       'creator' => { from: ['creator'], split: true },
+       'contributor' => { from: ['contributor'], split: true },
+       'description' => { from: ['description'], split: true },
+       'abstract' => { from: ['abstract'], split: true },
+       'keyword' => { from: ['keyword'], split: true },
+       'license' => { from: ['license'], split: '\|' },
+       'rights_notes' => { from: ['rights_notes'], split: true },
+       'rights_statement' => { from: ['rights_statement'], split: '\|' },
+       'access_right' => { from: ['access_right'], split: true },
+       'publisher' => { from: ['publisher'], split: true },
+       'date_created' => { from: ['date_created'], split: true },
+       'subject' => { from: ['subject'], split: true },
+       'language' => { from: ['language'], split: true },
+       'identifier' => { from: ['identifier'], split: '\|' },
+       'based_near' => { from: ['based_near'], split: true },
+       'related_url' => { from: ['related_url'], split: '\|'},
+       'bibliographic_citation' => { from: ['bibliographic_citation'], split: '\|' },
+       'source' => {from: ['source'], split: '\|' },
+       # from Hyrax::CoreMetadata
+       'title' => { from: ['title'], split: true },
+       'depositor' => { from: ['depositor'], split: true },
+       'date_uploaded' => { from: ['date_uploaded'], split: true },
+       'date_modified' => { from: ['date_modified'], split: true },
+       # from Hyrax::AdminSet
+       'alternative_title' => { from: ['alternative_title'], split: true },
+       # from GwWork
+       'bulkrax_identifier' => { from: ['bulkrax_identifier'], source_identifier: true },
+       'gw_affiliation' => { from: ['gw_affiliation'], split: true },
+       'doi' => { from: ['doi'], split: '\|' },
+       # from GwEtd
+       'degree' => { from: ['degree'], split: true },
+       'advisor' => { from: ['advisor'], split: true },
+       'committee_member' => { from: ['committee_member'], split: true },
+       'proquest_zipfile' => {from: ['proquest_zipfile'], split: true },
+       # from GwJournalIssue
+       'volume' => {from: ['volume'], split: true },
+       'issue' => {from: ['issue'], split: true },
+       # needed for Bulkrax
+       'file' => { from: ['file'], split: '\;' },
+       'parents' => { from: ['parents'], split: '\;', related_parents_field_mapping: true },
+     }
+    #
+    #
     # Create a completely new set of mappings by replacing the whole set as follows
     #   config.field_mappings = {
     #     "Bulkrax::OaiDcParser" => { **individual field mappings go here*** }
