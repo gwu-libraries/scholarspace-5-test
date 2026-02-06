b3048fe009acb80f62f750f989240a2dde5208f1
alex <alexanderleeboyd@gmail.com>
2026-02-06 13:58:51 -0500

    remove unused specs

--- spec/views/hyrax/derived_pages/_page.html.erb_spec.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-# Generated via
-#  `rails generate hyrax:work_resource Page`
-RSpec.describe "derived_pages/derived_page.html.erb", type: :view do # rubocop:disable  RSpec/EmptyExampleGroup
-  # add page view specs
-end
