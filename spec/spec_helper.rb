3a7d90d945c1ce874fb0210e266569a2eba4e2ef
alex <alexanderleeboyd@gmail.com>
2026-02-18 16:06:52 -0500

    remove unused TestHydraGroupService class from spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,44 +1,6 @@
-require "factory_bot_rails"
-
-class TestHydraGroupService
-  ##
-  # @param group_map [Hash{String, Array<String>}] map user keys to group names
-  def initialize(group_map: {})
-    @group_map = group_map
-  end
-
-  ##
-  # @param user [::User]
-  # @param groups [Array<String>, String]
-  #
-  # @return [void]
-  def add(user:, groups:)
-    @group_map[user.user_key] = fetch_groups(user: user) + Array(groups)
-  end
-
-  ##
-  # @return [void]
-  def clear
-    @group_map = {}
-  end
-
-  ##
-  # @param user [::User]
-  #
-  # @return [Array<String>]
-  def fetch_groups(user:)
-    @group_map.fetch(user.user_key) { [] }
-  end
-
-  ##
-  # @return [Array<String>] a list of all known group names
-  def role_names
-    @group_map.values.flatten.uniq
-  end
-end
+require 'factory_bot_rails'
 
 RSpec.configure do |config|
-  config.before(:suite) { User.group_service = TestHydraGroupService.new }
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
@@ -48,9 +10,9 @@ RSpec.configure do |config|
   config.shared_context_metadata_behavior = :apply_to_host_groups
 
   config.filter_run_when_matching :focus
-  config.example_status_persistence_file_path = "spec/examples.txt"
+  config.example_status_persistence_file_path = 'spec/examples.txt'
   config.disable_monkey_patching!
-  config.default_formatter = "doc" if config.files_to_run.one?
+  config.default_formatter = 'doc' if config.files_to_run.one?
   config.profile_examples = 10
   config.order = :random
   Kernel.srand config.seed
