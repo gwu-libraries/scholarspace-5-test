e02b3d59d021466e4984fe6b35a9fb7bfe0e0426
alex <alexanderleeboyd@gmail.com>
2026-02-18 16:04:42 -0500

    add todo, remove derived page mention in ocr search

--- app/services/hyrax/custom_queries/find_by_ocr_text_and_parent_document_id.rb
+++ app/services/hyrax/custom_queries/find_by_ocr_text_and_parent_document_id.rb
@@ -15,6 +15,7 @@ module Hyrax
       end
 
       attr_reader :query_service
+
       delegate :resource_factory, to: :query_service
       delegate :orm_class, to: :resource_factory
 
@@ -32,12 +33,13 @@ module Hyrax
         @parent_document_id = parent_document_id
 
         # ALEX - Does this need to take rows/pages params
-        @connection.get("select", params: { q: query, fl: "*", rows: 50 })
+        @connection.get('select', params: { q: query, fl: '*', rows: 50 })
       end
 
       # Solr query for for a Publication with a deduplication_key_tesi that matches the provided key
       # @return [Hash]
       def query
+        # TODO: Fix for derivedpage no longer being a model
         "ocr_text:#{@ocr_text} AND has_model_ssim:DerivedPage AND (is_page_of_ssim:#{@parent_document_id} OR id:#{@parent_document_id})"
       end
     end
