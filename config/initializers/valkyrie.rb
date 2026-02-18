5c8fc07c2ce70137965a44d9f77eedfdc23dcd8b
alex <alexanderleeboyd@gmail.com>
2026-02-18 16:05:34 -0500

    clean up valkyrie adapter initializers

--- config/initializers/valkyrie.rb
+++ config/initializers/valkyrie.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
-require "faraday/multipart"
+
+require 'faraday/multipart'
 
 # require "shrine/storage/s3"
 # require "valkyrie/storage/shrine"
@@ -33,7 +34,7 @@ Valkyrie::MetadataAdapter.register(
     connection:
       ::Ldp::Client.new(
         Hyrax.config.fedora_connection_builder.call(
-          ENV.fetch("FEDORA_URL") { "http://localhost:8080/fcrepo/rest" }
+          ENV.fetch('FEDORA_URL') { 'http://localhost:8080/fcrepo/rest' }
         )
       ),
     base_path: Rails.env, # sets to '/development' instead of '/dev'
@@ -48,34 +49,12 @@ Valkyrie::MetadataAdapter.register(
   :fedora_metadata
 )
 
-Valkyrie.config.metadata_adapter =
-  ENV.fetch("VALKYRIE_METADATA_ADAPTER") { :fedora_metadata }.to_sym
-
-# shrine_s3_options = {
-#   bucket: ENV.fetch("REPOSITORY_S3_BUCKET") { "scholarspace_pg#{Rails.env}" },
-#   region: ENV.fetch("REPOSITORY_S3_REGION", "us-east-1"),
-#   access_key_id: (ENV["REPOSITORY_S3_ACCESS_KEY"] || ENV["MINIO_ACCESS_KEY"]),
-#   secret_access_key: (ENV["REPOSITORY_S3_SECRET_KEY"] || ENV["MINIO_SECRET_KEY"])
-# }
-#
-# if ENV["MINIO_ENDPOINT"].present?
-#   shrine_s3_options[:endpoint] = "http://#{ENV["MINIO_ENDPOINT"]}:#{ENV.fetch("MINIO_PORT", 9000)}"
-#   shrine_s3_options[:force_path_style] = true
-# end
-#
-# Valkyrie::StorageAdapter.register(
-#   Valkyrie::Storage::Shrine.new(Shrine::Storage::S3.new(**shrine_s3_options)),
-#   :repository_s3
-# )
-#
-# Valkyrie.config.storage_adapter = :repository_s3
-
 Valkyrie::StorageAdapter.register(
   Valkyrie::Storage::Fedora.new(
     connection:
       ::Ldp::Client.new(
         Hyrax.config.fedora_connection_builder.call(
-          ENV.fetch("FEDORA_URL") { "http://localhost:8080/fcrepo/rest" }
+          ENV.fetch('FEDORA_URL') { 'http://localhost:8080/fcrepo/rest' }
         )
       ),
     base_path: Rails.env, # sets to '/development' instead of '/dev'
@@ -86,15 +65,6 @@ Valkyrie::StorageAdapter.register(
   :fedora_storage
 )
 
-Valkyrie::StorageAdapter.register(
-  Valkyrie::Storage::VersionedDisk.new(
-    base_path: Rails.root.join("storage", "files"),
-    file_mover: FileUtils.method(:cp)
-  ),
-  :versioned_disk_storage
-)
-
-Valkyrie.config.storage_adapter =
-  ENV.fetch("VALKYRIE_STORAGE_ADAPTER") { :fedora_storage }.to_sym
-
+Valkyrie.config.metadata_adapter = :fedora_metadata
+Valkyrie.config.storage_adapter = :fedora_storage
 Valkyrie.config.indexing_adapter = :solr_index
