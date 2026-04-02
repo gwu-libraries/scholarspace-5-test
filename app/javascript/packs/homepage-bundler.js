b19112331ab8ea2dd170a8206971184647df1ce9
alex <alexanderleeboyd@gmail.com>
2026-04-01 20:45:49 -0400

    add javascript/packs bundles

--- app/javascript/packs/homepage-bundler.js
+++ app/javascript/packs/homepage-bundler.js
@@ -1,10 +1,10 @@
-import ReactOnRails from "react-on-rails";
-// import Homepage from '../bundles/homepage/Homepage';
+import ReactOnRails from 'react-on-rails';
+import Homepage from '../bundles/homepage/Homepage';
 
 ReactOnRails.register({
   Homepage,
 });
 
-document.addEventListener("turbo:render", () => {
+document.addEventListener('turbo:render', () => {
   ReactOnRails.reactOnRailsPageLoaded();
 });
--- app/javascript/packs/work-show-bundler.js
+++ app/javascript/packs/work-show-bundler.js
@@ -1,10 +1,10 @@
 import ReactOnRails from "react-on-rails";
-// import WorkShow from "../bundles/work_show/WorkShow";
+import WorkShow from "../bundles/work_show/WorkShow";
 import "video.js/dist/video-js.css";
 import "@samvera/ramp/dist/ramp.css";
 
 ReactOnRails.register({
-  // WorkShow,
+  WorkShow,
 });
 
 document.addEventListener("turbo:render", () => {
