85fbf39332ee372972679ada22970f026bc5c54e
alex <alexanderleeboyd@gmail.com>
2026-04-02 21:21:25 -0400

    add ocr and vtt -able to work indexers

--- app/indexers/academic_document_indexer.rb
+++ app/indexers/academic_document_indexer.rb
@@ -7,6 +7,8 @@ class AcademicDocumentIndexer < Hyrax::Indexers.PcdmObjectIndexer(
 )
   include Hyrax.Indexer(:basic_metadata)
   include Hyrax.Indexer(:academic_document)
+  include OcrTextIndexable
+  include VttIndexable
 
   # Uncomment this block if you want to add custom indexing behavior:
   #  def to_solr
