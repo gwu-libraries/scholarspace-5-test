87ef63552de2664b5b1735f30a45c9e5e11ff66d
alex <alexanderleeboyd@gmail.com>
2026-04-15 14:00:31 -0400

    helper for determining viewer option on work show

--- app/helpers/work_viewer_helper.rb
+++ app/helpers/work_viewer_helper.rb
@@ -1,13 +1,11 @@
 module WorkViewerHelper
   # determines the default viewer tab for a work based on its file set members.
-  # returns 'images' when unprocessed source images are present, otherwise 'pdf'.
   def default_viewer_for_work(presenter)
     has_source_image_files?(presenter) ? 'images' : 'pdf'
   end
 
   def pdf_viewer_file_id_for_work(presenter)
-    preferred_rendering_pdf_file_set_id_for_work(presenter) ||
-      pdf_presenter_for_work(presenter)&.id ||
+    pdf_presenter_for_work(presenter)&.id ||
       pdf_file_set_id_for_work(presenter)
   end
 
@@ -17,6 +15,14 @@ module WorkViewerHelper
 
   def pdf_download_url_for_file(file_id)
     download_path(id: file_id, locale: nil)
+  end
+
+  def hocr_download_url_for_work(presenter, pdf_file_id: nil)
+    pdf_file_id ||= pdf_viewer_file_id_for_work(presenter)
+    hocr_file_id = hocr_file_set_id_for_pdf(presenter, pdf_file_id)
+    return nil if hocr_file_id.blank?
+
+    download_path(id: hocr_file_id, locale: nil)
   end
 
   private
@@ -37,15 +43,35 @@ module WorkViewerHelper
     nil
   end
 
-  def preferred_rendering_pdf_file_set_id_for_work(presenter)
-    Array(presenter.member_ids).each do |member_id|
-      file_set = find_member_file_set(member_id)
-      next unless file_set
+  def hocr_file_set_id_for_pdf(presenter, pdf_file_id)
+    return nil if pdf_file_id.blank?
 
-      filename = file_set.original_file&.original_filename.to_s
-      return file_set.id.to_s if filename.downcase.end_with?('_ocr_rendering.pdf')
+    pdf_file_set = find_member_file_set(pdf_file_id)
+    return nil unless pdf_file_set
+
+    expected_name = expected_hocr_filename_for_pdf_file_set(pdf_file_set)
+    member_file_sets = Array(presenter.member_ids).filter_map { |id| find_member_file_set(id) }
+
+    if expected_name.present?
+      exact_match = member_file_sets.find do |file_set|
+        filename = file_set.original_file&.original_filename.to_s
+        filename.casecmp(expected_name).zero?
+      end
+      return exact_match.id.to_s if exact_match
     end
-    nil
+
+    fallback = member_file_sets.find do |file_set|
+      filename = file_set.original_file&.original_filename.to_s
+      filename.downcase.end_with?('.hocr')
+    end
+    fallback&.id&.to_s
+  end
+
+  def expected_hocr_filename_for_pdf_file_set(pdf_file_set)
+    filename = pdf_file_set.original_file&.original_filename.to_s
+    return nil if filename.blank?
+
+    "#{File.basename(filename, File.extname(filename))}_HOCR.hocr"
   end
 
   def has_source_image_files?(presenter)
