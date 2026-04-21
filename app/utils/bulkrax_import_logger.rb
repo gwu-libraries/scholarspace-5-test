cf1efc3ed6d22091131b89628f3e9915bc107be9
Dolsy Smith <dsmith@gwu.edu>
2026-04-21 12:27:52 +0000

    Applying patch

--- app/utils/bulkrax_import_logger.rb
+++ app/utils/bulkrax_import_logger.rb
@@ -1,18 +1,27 @@
 # Defines a custom logger for use in logging to a file during automated Bulkrax ingests
+require 'json'
+
 class BulkraxImportLogger
   class << self
 
     def info(*args)
       new.info(*args)
     end
+
+    def debug(*args)
+      new.debug(*args)
+    end
+
   end
 
-  delegate :info, to: :logger
+  delegate :info, :debug, to: :logger
 
   attr_reader :logger
 
   def initialize
     @logger = ActiveSupport::Logger.new(Rails.root.join("log/bulkrax_imports.log"))
-    @logger.formatter = ::Logger::Formatter.new
+    @logger.formatter = proc do |severity, datetime, progname, msg|
+      {timestamp: datetime, message: msg}.to_json + "\n"
+    end
   end
 end
