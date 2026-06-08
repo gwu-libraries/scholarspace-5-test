cffa83c3cff07cee5cda54425e6204c616f96b95
alex <alexanderleeboyd@gmail.com>
2026-06-08 14:58:20 -0400

    turn off s3 versioning

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -10,7 +10,7 @@ resource "aws_s3_bucket_versioning" "app_bucket_versioning" {
   bucket = aws_s3_bucket.app_bucket.id
 
   versioning_configuration {
-    status = "Enabled"
+    status = "Disabled"
   }
 }
 
