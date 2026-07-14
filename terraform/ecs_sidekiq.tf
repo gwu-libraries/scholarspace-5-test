958d5115231e4cef431f17c45db2b20a846184c2
alex <alexanderleeboyd@gmail.com>
2026-07-13 22:11:04 -0400

    ecs scaling setup

---
+++ terraform/ecs_sidekiq.tf
@@ -0,0 +1,366 @@
+locals {
+  sidekiq_default_image_uri  = trimspace(var.sidekiq_default_image)
+  sidekiq_whisper_image_uri  = trimspace(var.sidekiq_whisper_image)
+  sidekiq_ocr_text_image_uri = trimspace(var.sidekiq_ocr_text_image)
+
+  sidekiq_service_configs = {
+    default = {
+      image                            = local.sidekiq_default_image_uri
+      sidekiq_only_audio_transcript    = "false"
+      sidekiq_only_ocr_text_extraction = "false"
+      sidekiq_only_derivatives         = "false"
+      sidekiq_only_thumbnail           = "false"
+      sidekiq_workers                  = "4"
+      desired_count                    = var.sidekiq_default_desired_count
+      min_capacity                     = var.sidekiq_default_min_capacity
+      max_capacity                     = var.sidekiq_default_max_capacity
+      cpu                              = var.sidekiq_task_cpu
+      memory                           = var.sidekiq_task_memory
+    }
+    whisper = {
+      image                            = local.sidekiq_whisper_image_uri
+      sidekiq_only_audio_transcript    = "true"
+      sidekiq_only_ocr_text_extraction = "false"
+      sidekiq_only_derivatives         = "false"
+      sidekiq_only_thumbnail           = "false"
+      sidekiq_workers                  = "1"
+      desired_count                    = var.sidekiq_whisper_desired_count
+      min_capacity                     = var.sidekiq_whisper_min_capacity
+      max_capacity                     = var.sidekiq_whisper_max_capacity
+      cpu                              = var.sidekiq_whisper_task_cpu
+      memory                           = var.sidekiq_whisper_task_memory
+    }
+    ocr_text = {
+      image                            = local.sidekiq_ocr_text_image_uri
+      sidekiq_only_audio_transcript    = "false"
+      sidekiq_only_ocr_text_extraction = "true"
+      sidekiq_only_derivatives         = "false"
+      sidekiq_only_thumbnail           = "false"
+      sidekiq_workers                  = "1"
+      desired_count                    = var.sidekiq_ocr_text_desired_count
+      min_capacity                     = var.sidekiq_ocr_text_min_capacity
+      max_capacity                     = var.sidekiq_ocr_text_max_capacity
+      cpu                              = var.sidekiq_ocr_text_task_cpu
+      memory                           = var.sidekiq_ocr_text_task_memory
+    }
+    images = {
+      image                            = local.sidekiq_default_image_uri
+      sidekiq_only_audio_transcript    = "false"
+      sidekiq_only_ocr_text_extraction = "false"
+      sidekiq_only_derivatives         = "true"
+      sidekiq_only_thumbnail           = "false"
+      sidekiq_workers                  = "2"
+      desired_count                    = var.sidekiq_derivatives_desired_count
+      min_capacity                     = var.sidekiq_derivatives_min_capacity
+      max_capacity                     = var.sidekiq_derivatives_max_capacity
+      cpu                              = var.sidekiq_derivatives_task_cpu
+      memory                           = var.sidekiq_derivatives_task_memory
+    }
+    thumbnail = {
+      image                            = local.sidekiq_default_image_uri
+      sidekiq_only_audio_transcript    = "false"
+      sidekiq_only_ocr_text_extraction = "false"
+      sidekiq_only_derivatives         = "false"
+      sidekiq_only_thumbnail           = "true"
+      sidekiq_workers                  = "2"
+      desired_count                    = var.sidekiq_thumbnail_desired_count
+      min_capacity                     = var.sidekiq_thumbnail_min_capacity
+      max_capacity                     = var.sidekiq_thumbnail_max_capacity
+      cpu                              = var.sidekiq_thumbnail_task_cpu
+      memory                           = var.sidekiq_thumbnail_task_memory
+    }
+  }
+
+  sidekiq_log_autoscaling_configs = var.sidekiq_enable_log_based_autoscaling ? local.sidekiq_service_configs : {}
+}
+
+resource "aws_cloudwatch_log_group" "sidekiq" {
+  for_each = local.sidekiq_service_configs
+
+  name              = "/ecs/${var.site_prefix}/sidekiq/${each.key}"
+  retention_in_days = var.sidekiq_log_retention_days
+
+  tags = {
+    Name = "${var.site_prefix}-sidekiq-${each.key}"
+  }
+}
+
+resource "aws_ecs_cluster" "sidekiq" {
+  name = "${var.site_prefix}-sidekiq"
+
+  setting {
+    name  = "containerInsights"
+    value = "enabled"
+  }
+
+  tags = {
+    Name = "${var.site_prefix}-sidekiq"
+  }
+}
+
+resource "aws_ecs_task_definition" "sidekiq" {
+  for_each = local.sidekiq_service_configs
+
+  family                   = "${var.site_prefix}-sidekiq-${each.key}"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = tostring(each.value.cpu)
+  memory                   = tostring(each.value.memory)
+  network_mode             = "awsvpc"
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+  task_role_arn            = aws_iam_role.ecs_task_role.arn
+
+  container_definitions = jsonencode([
+    {
+      name        = "sidekiq-${each.key}"
+      image       = each.value.image
+      essential   = true
+      stopTimeout = var.sidekiq_container_stop_timeout_seconds
+      command = [
+        "sh",
+        "-lc",
+        "exec bundle exec sidekiq -t ${var.sidekiq_shutdown_timeout_seconds}"
+      ]
+      environment = concat(local.ecs_common_container_environment, [
+        { name = "SIDEKIQ_ONLY_AUDIO_TRANSCRIPT", value = each.value.sidekiq_only_audio_transcript },
+        { name = "SIDEKIQ_ONLY_OCR_TEXT_EXTRACTION", value = each.value.sidekiq_only_ocr_text_extraction },
+        { name = "SIDEKIQ_ONLY_DERIVATIVES", value = each.value.sidekiq_only_derivatives },
+        { name = "SIDEKIQ_ONLY_THUMBNAIL", value = each.value.sidekiq_only_thumbnail },
+        { name = "SIDEKIQ_WORKERS", value = each.value.sidekiq_workers }
+      ])
+      secrets = local.ecs_common_container_secrets
+      mountPoints = [
+        local.ecs_uploads_mount_point,
+        {
+          sourceVolume  = "ocr-cache"
+          containerPath = "/app/scholarspace/tmp/cache/solr-ocr-index-cache"
+          readOnly      = false
+        },
+        {
+          sourceVolume  = "derivatives-cache"
+          containerPath = "/app/scholarspace/tmp/cache/derivatives"
+          readOnly      = false
+        }
+      ]
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          awslogs-group         = aws_cloudwatch_log_group.sidekiq[each.key].name
+          awslogs-region        = var.aws_region
+          awslogs-stream-prefix = each.key
+        }
+      }
+    }
+  ])
+
+  volume {
+    name = "uploads"
+
+    efs_volume_configuration {
+      file_system_id     = aws_efs_file_system.uploads.id
+      root_directory     = "/"
+      transit_encryption = "ENABLED"
+
+      authorization_config {
+        access_point_id = aws_efs_access_point.uploads.id
+        iam             = "DISABLED"
+      }
+    }
+  }
+
+  volume {
+    name = "ocr-cache"
+
+    efs_volume_configuration {
+      file_system_id     = aws_efs_file_system.uploads.id
+      root_directory     = "/"
+      transit_encryption = "ENABLED"
+
+      authorization_config {
+        access_point_id = aws_efs_access_point.ocr_cache.id
+        iam             = "DISABLED"
+      }
+    }
+  }
+
+  volume {
+    name = "derivatives-cache"
+
+    efs_volume_configuration {
+      file_system_id     = aws_efs_file_system.uploads.id
+      root_directory     = "/"
+      transit_encryption = "ENABLED"
+
+      authorization_config {
+        access_point_id = aws_efs_access_point.derivatives_cache.id
+        iam             = "DISABLED"
+      }
+    }
+  }
+}
+
+resource "aws_ecs_service" "sidekiq" {
+  for_each = local.sidekiq_service_configs
+
+  name                   = "${var.site_prefix}-sidekiq-${each.key}"
+  cluster                = aws_ecs_cluster.sidekiq.id
+  task_definition        = aws_ecs_task_definition.sidekiq[each.key].arn
+  desired_count          = each.value.desired_count
+  launch_type            = "FARGATE"
+  enable_execute_command = true
+
+  deployment_minimum_healthy_percent = 50
+  deployment_maximum_percent         = 200
+
+  deployment_circuit_breaker {
+    enable   = true
+    rollback = true
+  }
+
+  network_configuration {
+    subnets          = [aws_subnet.app_private_subnet.id, aws_subnet.app_private_subnet_secondary.id]
+    security_groups  = [aws_security_group.sidekiq_tasks.id]
+    assign_public_ip = var.sidekiq_assign_public_ip
+  }
+
+  depends_on = [
+    aws_security_group_rule.aurora_from_sidekiq_tasks,
+    aws_rds_cluster_instance.aurora,
+    aws_ecs_service.memcached,
+    aws_ecs_service.fits,
+    aws_ecs_service.fedora,
+    aws_ecs_service.solr,
+  ]
+}
+
+resource "aws_appautoscaling_target" "sidekiq" {
+  for_each = local.sidekiq_service_configs
+
+  max_capacity       = each.value.max_capacity
+  min_capacity       = each.value.min_capacity
+  resource_id        = "service/${aws_ecs_cluster.sidekiq.name}/${aws_ecs_service.sidekiq[each.key].name}"
+  scalable_dimension = "ecs:service:DesiredCount"
+  service_namespace  = "ecs"
+}
+
+resource "aws_appautoscaling_policy" "sidekiq_cpu" {
+  for_each = local.sidekiq_service_configs
+
+  name               = "${var.site_prefix}-sidekiq-${each.key}-cpu"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.sidekiq[each.key].resource_id
+  scalable_dimension = aws_appautoscaling_target.sidekiq[each.key].scalable_dimension
+  service_namespace  = aws_appautoscaling_target.sidekiq[each.key].service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ECSServiceAverageCPUUtilization"
+    }
+    target_value       = var.sidekiq_target_cpu_utilization
+    scale_in_cooldown  = 120
+    scale_out_cooldown = 120
+  }
+}
+
+resource "aws_appautoscaling_policy" "sidekiq_memory" {
+  for_each = local.sidekiq_service_configs
+
+  name               = "${var.site_prefix}-sidekiq-${each.key}-memory"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.sidekiq[each.key].resource_id
+  scalable_dimension = aws_appautoscaling_target.sidekiq[each.key].scalable_dimension
+  service_namespace  = aws_appautoscaling_target.sidekiq[each.key].service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ECSServiceAverageMemoryUtilization"
+    }
+    target_value       = var.sidekiq_target_memory_utilization
+    scale_in_cooldown  = 120
+    scale_out_cooldown = 120
+  }
+}
+
+resource "aws_cloudwatch_log_metric_filter" "sidekiq_queue_pressure" {
+  for_each = local.sidekiq_log_autoscaling_configs
+
+  name           = "${var.site_prefix}-sidekiq-${each.key}-queue-pressure"
+  log_group_name = aws_cloudwatch_log_group.sidekiq[each.key].name
+  pattern        = var.sidekiq_log_autoscaling_pattern
+
+  metric_transformation {
+    name          = "${var.site_prefix}-sidekiq-${each.key}-queue-pressure"
+    namespace     = "${var.site_prefix}/Sidekiq"
+    value         = "1"
+    default_value = 0
+  }
+}
+
+resource "aws_cloudwatch_log_metric_filter" "sidekiq_queue_depth_pressure" {
+  for_each = local.sidekiq_log_autoscaling_configs
+
+  name           = "${var.site_prefix}-sidekiq-${each.key}-queue-depth-pressure"
+  log_group_name = aws_cloudwatch_log_group.sidekiq[each.key].name
+  pattern        = var.sidekiq_log_autoscaling_depth_pattern
+
+  metric_transformation {
+    name          = "${var.site_prefix}-sidekiq-${each.key}-queue-depth-pressure"
+    namespace     = "${var.site_prefix}/Sidekiq"
+    value         = "1"
+    default_value = 0
+  }
+}
+
+resource "aws_appautoscaling_policy" "sidekiq_log_scale_out" {
+  for_each = local.sidekiq_log_autoscaling_configs
+
+  name               = "${var.site_prefix}-sidekiq-${each.key}-log-scale-out"
+  policy_type        = "StepScaling"
+  resource_id        = aws_appautoscaling_target.sidekiq[each.key].resource_id
+  scalable_dimension = aws_appautoscaling_target.sidekiq[each.key].scalable_dimension
+  service_namespace  = aws_appautoscaling_target.sidekiq[each.key].service_namespace
+
+  step_scaling_policy_configuration {
+    adjustment_type         = "ChangeInCapacity"
+    cooldown                = var.sidekiq_log_scale_out_cooldown_seconds
+    metric_aggregation_type = "Maximum"
+
+    step_adjustment {
+      metric_interval_lower_bound = 0
+      scaling_adjustment          = var.sidekiq_log_scale_out_adjustment
+    }
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "sidekiq_log_scale_out" {
+  for_each = local.sidekiq_log_autoscaling_configs
+
+  alarm_name          = "${var.site_prefix}-sidekiq-${each.key}-log-scale-out"
+  alarm_description   = "Scale out Sidekiq ${each.key} service when log queue pressure events increase"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = var.sidekiq_log_autoscaling_evaluation_periods
+  threshold           = var.sidekiq_log_autoscaling_threshold
+  metric_name         = aws_cloudwatch_log_metric_filter.sidekiq_queue_pressure[each.key].metric_transformation[0].name
+  namespace           = "${var.site_prefix}/Sidekiq"
+  period              = var.sidekiq_log_autoscaling_period_seconds
+  statistic           = "Sum"
+  treat_missing_data  = "notBreaching"
+  alarm_actions       = [aws_appautoscaling_policy.sidekiq_log_scale_out[each.key].arn]
+  ok_actions          = []
+}
+
+resource "aws_cloudwatch_metric_alarm" "sidekiq_log_depth_scale_out" {
+  for_each = local.sidekiq_log_autoscaling_configs
+
+  alarm_name          = "${var.site_prefix}-sidekiq-${each.key}-log-depth-scale-out"
+  alarm_description   = "Scale out Sidekiq ${each.key} service when log queue depth pressure events increase"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = var.sidekiq_log_autoscaling_evaluation_periods
+  threshold           = var.sidekiq_log_autoscaling_depth_threshold
+  metric_name         = aws_cloudwatch_log_metric_filter.sidekiq_queue_depth_pressure[each.key].metric_transformation[0].name
+  namespace           = "${var.site_prefix}/Sidekiq"
+  period              = var.sidekiq_log_autoscaling_period_seconds
+  statistic           = "Sum"
+  treat_missing_data  = "notBreaching"
+  alarm_actions       = [aws_appautoscaling_policy.sidekiq_log_scale_out[each.key].arn]
+  ok_actions          = []
+}
---
+++ terraform/ecs_solr.tf
@@ -0,0 +1,236 @@
+locals {
+  solr_image_uri = length(trimspace(var.solr_image)) > 0 ? var.solr_image : "solr:9.6"
+}
+
+resource "aws_cloudwatch_log_group" "solr" {
+  name              = "/ecs/${var.site_prefix}/solr"
+  retention_in_days = var.solr_log_retention_days
+
+  tags = {
+    Name = "${var.site_prefix}-solr"
+  }
+}
+
+resource "aws_service_discovery_service" "solr" {
+  name = "solr"
+
+  dns_config {
+    namespace_id = aws_service_discovery_private_dns_namespace.internal.id
+
+    dns_records {
+      ttl  = 10
+      type = "A"
+    }
+
+    routing_policy = "MULTIVALUE"
+  }
+
+  health_check_custom_config {
+    failure_threshold = 1
+  }
+}
+
+resource "aws_security_group" "solr_tasks" {
+  name        = "${var.site_prefix}-solr-tasks"
+  description = "Ingress from web/sidekiq ECS tasks to Solr"
+  vpc_id      = aws_vpc.app_vpc.id
+
+  ingress {
+    description     = "Allow Rails web ECS tasks to reach Solr"
+    from_port       = 8983
+    to_port         = 8983
+    protocol        = "tcp"
+    security_groups = [aws_security_group.web_tasks.id]
+  }
+
+  ingress {
+    description     = "Allow Sidekiq ECS tasks to reach Solr"
+    from_port       = 8983
+    to_port         = 8983
+    protocol        = "tcp"
+    security_groups = [aws_security_group.sidekiq_tasks.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.site_prefix}-solr-tasks"
+  }
+}
+
+resource "aws_ecs_task_definition" "solr" {
+  family                   = "${var.site_prefix}-solr"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = tostring(var.solr_task_cpu)
+  memory                   = tostring(var.solr_task_memory)
+  network_mode             = "awsvpc"
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+  task_role_arn            = aws_iam_role.ecs_task_role.arn
+
+  container_definitions = jsonencode([
+    {
+      name      = "solr"
+      image     = local.solr_image_uri
+      essential = true
+      command = [
+        "sh",
+        "-lc",
+        "test -d /opt/solr/server/configsets/hyraxconf || { echo 'Missing hyraxconf configset. Build and set solr_image from Dockerfile-solr.'; exit 1; } && mkdir -p /var/solr/data && precreate-core scholarspace_test /opt/solr/server/configsets/hyraxconf && exec solr-precreate scholarspace_prod /opt/solr/server/configsets/hyraxconf"
+      ]
+      environment = [
+        { name = "SOLR_HEAP", value = var.solr_heap }
+      ]
+      portMappings = [
+        {
+          containerPort = 8983
+          hostPort      = 8983
+          protocol      = "tcp"
+        }
+      ]
+      mountPoints = [
+        {
+          sourceVolume  = "solr-data"
+          containerPath = "/var/solr"
+          readOnly      = false
+        },
+        {
+          sourceVolume  = "ocr-cache"
+          containerPath = "/app/scholarspace/tmp/cache/solr-ocr-index-cache"
+          readOnly      = true
+        }
+      ]
+      healthCheck = {
+        command     = ["CMD-SHELL", "curl -fsS http://localhost:8983/solr/admin/info/system || exit 1"]
+        interval    = 30
+        timeout     = 10
+        retries     = 5
+        startPeriod = 90
+      }
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          awslogs-group         = aws_cloudwatch_log_group.solr.name
+          awslogs-region        = var.aws_region
+          awslogs-stream-prefix = "solr"
+        }
+      }
+    }
+  ])
+
+  volume {
+    name = "solr-data"
+
+    efs_volume_configuration {
+      file_system_id     = aws_efs_file_system.uploads.id
+      root_directory     = "/"
+      transit_encryption = "ENABLED"
+
+      authorization_config {
+        access_point_id = aws_efs_access_point.solr_data.id
+        iam             = "DISABLED"
+      }
+    }
+  }
+
+  volume {
+    name = "ocr-cache"
+
+    efs_volume_configuration {
+      file_system_id     = aws_efs_file_system.uploads.id
+      root_directory     = "/"
+      transit_encryption = "ENABLED"
+
+      authorization_config {
+        access_point_id = aws_efs_access_point.ocr_cache.id
+        iam             = "DISABLED"
+      }
+    }
+  }
+}
+
+resource "aws_ecs_service" "solr" {
+  name                   = "${var.site_prefix}-solr"
+  cluster                = aws_ecs_cluster.sidekiq.id
+  task_definition        = aws_ecs_task_definition.solr.arn
+  desired_count          = var.solr_desired_count
+  launch_type            = "FARGATE"
+  enable_execute_command = true
+
+  # Solr is configured as a single-node core on shared storage. Avoid overlapping
+  # old/new tasks during deployments to prevent concurrent writers.
+  deployment_minimum_healthy_percent = 0
+  deployment_maximum_percent         = 100
+
+  deployment_circuit_breaker {
+    enable   = true
+    rollback = true
+  }
+
+  network_configuration {
+    subnets          = [aws_subnet.app_private_subnet.id, aws_subnet.app_private_subnet_secondary.id]
+    security_groups  = [aws_security_group.solr_tasks.id]
+    assign_public_ip = var.solr_assign_public_ip
+  }
+
+  service_registries {
+    registry_arn = aws_service_discovery_service.solr.arn
+  }
+
+  lifecycle {
+    precondition {
+      condition = (
+        var.solr_desired_count <= 1 &&
+        var.solr_min_capacity <= 1 &&
+        var.solr_max_capacity <= 1
+      )
+      error_message = "Standalone Solr on shared EFS must run as a single instance."
+    }
+  }
+}
+
+resource "aws_appautoscaling_target" "solr" {
+  max_capacity       = var.solr_max_capacity
+  min_capacity       = var.solr_min_capacity
+  resource_id        = "service/${aws_ecs_cluster.sidekiq.name}/${aws_ecs_service.solr.name}"
+  scalable_dimension = "ecs:service:DesiredCount"
+  service_namespace  = "ecs"
+}
+
+resource "aws_appautoscaling_policy" "solr_cpu" {
+  name               = "${var.site_prefix}-solr-cpu"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.solr.resource_id
+  scalable_dimension = aws_appautoscaling_target.solr.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.solr.service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ECSServiceAverageCPUUtilization"
+    }
+    target_value       = var.solr_target_cpu_utilization
+    scale_in_cooldown  = 120
+    scale_out_cooldown = 120
+  }
+}
+
+resource "aws_appautoscaling_policy" "solr_memory" {
+  name               = "${var.site_prefix}-solr-memory"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.solr.resource_id
+  scalable_dimension = aws_appautoscaling_target.solr.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.solr.service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ECSServiceAverageMemoryUtilization"
+    }
+    target_value       = var.solr_target_memory_utilization
+    scale_in_cooldown  = 120
+    scale_out_cooldown = 120
+  }
+}
