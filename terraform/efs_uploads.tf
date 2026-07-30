56511d1d947ff60604f4ac01104ccf22bd2878fe
alex <alexanderleeboyd@gmail.com>
2026-07-30 14:06:54 -0400

    move efs solr_data into its own file for consistency

--- terraform/efs_uploads.tf
+++ terraform/efs_uploads.tf
@@ -91,30 +91,3 @@ resource "aws_efs_mount_target" "uploads_secondary" {
   security_groups = [aws_security_group.efs_uploads.id]
 }
 
-resource "aws_efs_access_point" "solr_data" {
-  file_system_id = aws_efs_file_system.uploads.id
-
-  lifecycle {
-    prevent_destroy = true
-  }
-
-  posix_user {
-    uid = 8983
-    gid = 8983
-  }
-
-  root_directory {
-    path = "/solr-data"
-
-    creation_info {
-      owner_gid   = 8983
-      owner_uid   = 8983
-      permissions = "0775"
-    }
-  }
-
-  tags = {
-    Name = "${var.site_prefix}-solr-data-access-point"
-  }
-}
-
