f0af2fcf483b7290dec9299bd1f3f4b36b551c5f
alex <alexanderleeboyd@gmail.com>
2026-08-04 12:28:05 -0400

    add derivative services constant concerns

---
+++ app/services/concerns/constants/thumbnail_filename_constants.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+module Constants
+  module ThumbnailFilenameConstants
+    REPRESENTATIVE_THUMBNAIL_FILENAME = 'REPRESENTATIVE_THUMBNAIL.jpg'
+    GENERATED_THUMBNAIL_SUFFIX = '_THUMBNAIL.jpg'
+  end
+end
