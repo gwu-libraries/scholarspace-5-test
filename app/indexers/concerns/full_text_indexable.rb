06925204803c56dca4230badbef061afab51850e
alex <alexanderleeboyd@gmail.com>
2026-08-04 18:08:42 -0400

    add indexer modules

---
+++ app/indexers/concerns/full_text_indexable.rb
@@ -0,0 +1,146 @@
+# frozen_string_literal: true
+
+require 'nokogiri'
+
+module FullTextIndexable
+  extend ActiveSupport::Concern
+  include MemberQueries
+  include StringNormalization
+
+  MAX_INDEX_VALUE_CHARS = 3000
+
+  def to_solr
+    super.tap do |index_document|
+      full_text = extract_full_text_content
+      append_plain_text_to_index(index_document, full_text) if full_text.present?
+    end
+  end
+
+  private
+
+  def extract_full_text_content
+    text_segments = resource.member_ids.filter_map do |member_id|
+      member = find_member_by_id(member_id)
+      next unless member
+
+      full_text_for_member(member)
+    end
+
+    text_segments.join("\n").presence
+  end
+
+  def full_text_for_member(member)
+    text_segments = []
+
+    file = member.original_file if member.respond_to?(:original_file)
+    if file
+      file_content = read_file_content(file)
+      if file_content.present?
+        extracted_file_text = extract_file_text(file: file, content: file_content)
+        text_segments << extracted_file_text if extracted_file_text.present?
+      end
+    end
+
+    extracted_text_content = extract_extracted_text_content(member)
+    text_segments << normalize_plain_text(extracted_text_content) if extracted_text_content.present?
+
+    text_segments.join("\n").presence
+  end
+
+  def extract_file_text(file:, content:)
+    filename = normalize_filename(file.original_filename)
+
+    if filename.end_with?('.hocr')
+      extract_hocr_plain_text(content)
+    end
+  end
+
+  def extract_extracted_text_content(member)
+    return unless member.respond_to?(:extracted_text_id)
+    return if member.extracted_text_id.blank?
+
+    Hyrax.custom_queries.find_extracted_text(file_set: member)&.content
+  rescue Valkyrie::Persistence::ObjectNotFoundError
+    nil
+  end
+
+  def extract_hocr_plain_text(content)
+    document = Nokogiri::HTML(content, nil, 'UTF-8')
+    words = document.css('span.ocrx_word, span.ocr_word').filter_map do |word_node|
+      normalize_plain_text(word_node.text)
+    end
+
+    return words.join(' ') if words.any?
+
+    normalize_plain_text(document.at('body')&.text || document.text)
+  end
+
+  def append_plain_text_to_index(index_document, plain_text)
+    existing_values = split_index_values(index_document)
+    combined_values = existing_values + split_plain_text_for_index(plain_text)
+    index_document[:all_text_tsimv] = combined_values
+  end
+
+  def split_index_values(index_document)
+    Array(index_document[:all_text_tsimv]).flat_map do |value|
+      split_plain_text_for_index(value)
+    end
+  end
+
+  def split_plain_text_for_index(text, max_chars: MAX_INDEX_VALUE_CHARS)
+    normalized_text = normalize_plain_text(text)
+    return [] if normalized_text.blank?
+
+    segments = []
+    current_segment = +''
+
+    normalized_text.split.each do |token|
+      if token.length > max_chars
+        segments << current_segment if current_segment.present?
+        current_segment = +''
+        token.scan(/.{1,#{max_chars}}/).each { |chunk| segments << chunk }
+        next
+      end
+
+      if current_segment.blank?
+        current_segment = token.dup
+      elsif current_segment.length + token.length + 1 <= max_chars
+        current_segment << " #{token}"
+      else
+        segments << current_segment
+        current_segment = token.dup
+      end
+    end
+
+    segments << current_segment if current_segment.present?
+    segments
+  end
+
+  def read_file_content(file)
+    return nil unless file.respond_to?(:file_identifier)
+
+    file_identifier = file.file_identifier
+    return nil if file_identifier.blank?
+
+    io = find_storage_file_by_id(file_identifier)
+    return nil unless io
+
+    io.stream.read
+  end
+
+  def normalize_plain_text(text)
+    sanitized = text.to_s.encode('UTF-8', invalid: :replace, undef: :replace, replace: ' ')
+    sanitized
+      .gsub(/[\u0000-\u0008\u000B\u000C\u000E-\u001F\u007F]/, ' ')
+      .unicode_normalize(:nfkc)
+      .gsub(/\s+/, ' ')
+      .strip
+      .presence
+  end
+
+  def find_storage_file_by_id(file_identifier)
+    Hyrax.storage_adapter.find_by(id: file_identifier)
+  rescue Valkyrie::Persistence::ObjectNotFoundError
+    nil
+  end
+end
---
+++ app/indexers/concerns/ocr_text_indexable.rb
@@ -0,0 +1,119 @@
+# frozen_string_literal: true
+
+require 'digest'
+require 'fileutils'
+
+module OcrTextIndexable
+  extend ActiveSupport::Concern
+  include Constants::DerivativeFilenameConstants
+  include MemberQueries
+  include FileOperations
+
+  # We are using https://dbmdz.github.io/solr-ocrhighlighting/latest/
+  # for ocr text indexing that includes bounding boxes. 
+
+  # essentially this points a field ("ocr_text") in the solr document 
+  # to a stored HOCR document.
+
+  # HOWEVER! because our hocr documents are being persisted in fedora on S3, in
+  # case they have been fixed or modified and we want to maintain those changes,
+  # we need them available on disk as well. This creates a copy of the HOCR
+  # document from fedora on local disk and updates the :ocr_text pointe in the 
+  # solr document. 
+
+  # this does create an additional hocr document in the cache whenever a new version
+  # of one is deposited, but it updates the pointer to the new doc. These won't be updated
+  # with much frequency and are just xml files, so ¯\_(ツ)_/¯
+
+  included do
+    # Override to_solr to include OCR source pointers for Solr OCR Highlighting.
+  end
+
+  def to_solr
+    super.tap do |index_document|
+      ocr_source_pointer = extract_ocr_source_pointer
+      index_document[:ocr_text] = ocr_source_pointer if ocr_source_pointer.present?
+    end
+  end
+
+  private
+
+  def extract_ocr_source_pointer
+    hocr_pointers = resource.member_ids.filter_map do |member_id|
+      member = find_member_by_id(member_id)
+      next unless member
+
+      files = hocr_files_for_member(member)
+      next if files.empty?
+
+      files.filter_map { |file| source_pointer_for(file) }
+    end.compact
+
+    hocr_pointers.join('+') if hocr_pointers.any?
+  end
+
+  def hocr_file?(file)
+    return false unless file.respond_to?(:original_filename)
+
+    filename = file.original_filename.to_s
+    return false if filename.empty?
+    return false if filename.casecmp?(JOINED_IMAGES_HOCR_FILENAME)
+
+    filename.downcase.end_with?('.hocr')
+  end
+
+  def hocr_files_for_member(member)
+    return [] unless member.respond_to?(:original_file)
+
+    file = member.original_file
+    return [] unless file && hocr_file?(file)
+
+    [file]
+  end
+
+  def source_pointer_for(file)
+    return nil unless file.respond_to?(:file_identifier)
+
+    file_identifier = file.file_identifier
+    return nil if file_identifier.blank?
+
+    file_identifier = file_identifier.to_s
+    pointer_path = ocr_pointer_path(
+      file_identifier: file_identifier,
+      original_filename: file.original_filename.to_s
+    )
+
+    copied = persist_ocr_file(file_identifier: file_identifier, pointer_path: pointer_path)
+    pointer_path if copied
+  end
+
+  def ocr_pointer_path(file_identifier:, original_filename:)
+    extension = File.extname(original_filename).presence || '.hocr'
+    digest = Digest::SHA256.hexdigest(file_identifier)
+    File.join(ocr_pointer_root, "#{digest}#{extension}")
+  end
+
+  def persist_ocr_file(file_identifier:, pointer_path:)
+    ensure_directory_exists(File.dirname(pointer_path))
+    copied_path = copy_file_to_disk(file_identifier, pointer_path)
+    copied_path.present?
+  end
+
+
+
+  def ocr_pointer_root
+    configured_root = '/app/scholarspace/tmp/cache/solr-ocr-index-cache'
+    return configured_root if cache_root_writable?(configured_root)
+
+    fallback_root = ENV.fetch('SOLR_OCR_INDEX_CACHE_ROOT', '/tmp/scholarspace-cache/solr-ocr-index-cache')
+    ensure_directory_exists(fallback_root)
+    fallback_root
+  end
+
+  def cache_root_writable?(path)
+    ensure_directory_exists(path)
+    File.writable?(path)
+  rescue SystemCallError
+    false
+  end
+end
