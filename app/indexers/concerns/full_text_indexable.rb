a521e4275c1af8b34e607727ff6dade2d4e7d0f7
alex <alexanderleeboyd@gmail.com>
2026-09-04 13:37:14 -0400

    fix up indexers, rename ocr_text to hocr_markup to indicate inclusion of bbox

--- app/indexers/concerns/full_text_indexable.rb
+++ app/indexers/concerns/full_text_indexable.rb
@@ -1,96 +1,30 @@
 # frozen_string_literal: true
 
-require 'nokogiri'
-require 'open3'
-require 'tempfile'
-
 module FullTextIndexable
   extend ActiveSupport::Concern
-  include MemberQueries
   include StringNormalization
 
   MAX_INDEX_VALUE_CHARS = DerivativeServiceSettings.fetch(:indexing, :max_index_value_chars)
 
   def to_solr
     super.tap do |index_document|
-      full_text = extract_full_text_content
-      append_plain_text_to_index(index_document, full_text) if full_text.present?
+      indexed_values = member_solr_documents.flat_map { |document| Array(document['all_text_tsimv']) }
+      append_plain_text_to_index(index_document, indexed_values.join("\n")) if indexed_values.present?
     end
   end
 
   private
 
-  def extract_full_text_content
-    text_segments = resource.member_ids.filter_map do |member_id|
-      member = find_member_by_id(member_id)
-      next unless member
+  def member_solr_documents
+    ids = Array(resource.member_ids).map(&:to_s)
+    return [] if ids.empty?
 
-      full_text_for_member(member)
-    end
-
-    text_segments.join("\n").presence
-  end
-
-  def full_text_for_member(member)
-    text_segments = []
-
-    file = member.original_file if member.respond_to?(:original_file)
-    if file
-      file_content = read_file_content(file)
-      if file_content.present?
-        extracted_file_text = extract_file_text(file: file, content: file_content)
-        text_segments << extracted_file_text if extracted_file_text.present?
-      end
-    end
-
-    extracted_text_content = extract_extracted_text_content(member)
-    text_segments << normalize_plain_text(extracted_text_content) if extracted_text_content.present?
-
-    text_segments.join("\n").presence
-  end
-
-  def extract_file_text(file:, content:)
-    filename = normalize_filename(file.original_filename)
-
-    return extract_hocr_plain_text(content) if filename.end_with?('.hocr')
-    return extract_pdf_plain_text(content) if filename.end_with?('.pdf')
-
-    nil
-  end
-
-  def extract_pdf_plain_text(content)
-    Tempfile.create(['scholarspace-index', '.pdf']) do |pdf_file|
-      pdf_file.binmode
-      pdf_file.write(content)
-      pdf_file.flush
-
-      stdout, _stderr, status = Open3.capture3('pdftotext', '-q', pdf_file.path, '-')
-      return nil unless status.success?
-
-      normalize_plain_text(stdout)
-    end
-  rescue StandardError
-    nil
-  end
-
-  def extract_extracted_text_content(member)
-    return unless member.respond_to?(:extracted_text_id)
-    return if member.extracted_text_id.blank?
-
-    Hyrax.custom_queries.find_extracted_text(file_set: member)&.content
-  rescue Valkyrie::Persistence::ObjectNotFoundError
-    nil
-  end
-
-  def extract_hocr_plain_text(content)
-    document = Nokogiri::HTML(content, nil, 'UTF-8')
-    words = document.css('span.ocrx_word, span.ocr_word').filter_map do |word_node|
-      normalize_plain_text(word_node.text)
-    end
-
-    return words.join(' ') if words.any?
-
-    normalize_plain_text(document.at('body')&.text || document.text)
+    response = Hyrax::SolrService.post(
+      "{!terms f=id}#{ids.join(',')}",
+      rows: ids.size,
+      fl: 'id,all_text_tsimv'
+    )
+    response.dig('response', 'docs') || []
   end
 
   def append_plain_text_to_index(index_document, plain_text)
@@ -134,18 +68,6 @@ module FullTextIndexable
     segments
   end
 
-  def read_file_content(file)
-    return nil unless file.respond_to?(:file_identifier)
-
-    file_identifier = file.file_identifier
-    return nil if file_identifier.blank?
-
-    io = find_storage_file_by_id(file_identifier)
-    return nil unless io
-
-    io.stream.read
-  end
-
   def normalize_plain_text(text)
     sanitized = text.to_s.encode('UTF-8', invalid: :replace, undef: :replace, replace: ' ')
     sanitized
@@ -156,9 +78,4 @@ module FullTextIndexable
       .presence
   end
 
-  def find_storage_file_by_id(file_identifier)
-    Hyrax.storage_adapter.find_by(id: file_identifier)
-  rescue Valkyrie::Persistence::ObjectNotFoundError
-    nil
-  end
 end
