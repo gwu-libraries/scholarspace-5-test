1a764075eacf1f40aa91f3c4bb9e7dec89bb5242
alex <alexanderleeboyd@gmail.com>
2026-09-09 16:42:52 -0400

    fix recursive fedora query situation

--- app/models/concerns/work_member_file_set_queries.rb
+++ app/models/concerns/work_member_file_set_queries.rb
@@ -3,9 +3,17 @@
 module WorkMemberFileSetQueries
   extend ActiveSupport::Concern
 
-  # Returns all member file sets for this work
   def member_file_sets
-    Array(member_ids).filter_map { |id| find_member_file_set(id) }
+    # The way I was handling this was a problem, causing recursive Fedora queries
+
+    # quite possibly should just be a solr query, but we may need to have the option to query from fedora
+  
+    ids = Array(member_ids).map(&:to_s)
+    return @member_file_sets_cache[:file_sets] if @member_file_sets_cache && @member_file_sets_cache[:ids] == ids
+
+    file_sets = ids.filter_map { |id| find_member_file_set(id) }
+    @member_file_sets_cache = { ids: ids, file_sets: file_sets }
+    file_sets
   end
 
   # Returns only non-service member file sets (original uploads, not derivatives)
@@ -18,21 +26,5 @@ module WorkMemberFileSetQueries
     Hyrax.query_service.find_by(id: file_set_id)
   rescue Valkyrie::Persistence::ObjectNotFoundError
     nil
-  end
-
-  # Check if all files have been characterized
-  def all_files_characterized?
-    child_works = member_ids.map { |id| Hyrax.query_service.find_by(id: id) }
-
-    child_works_metadata =
-      child_works.map do |cw|
-        cw&.file_ids&.first&.then do |file_id|
-          Hyrax.custom_queries.find_file_metadata_by(id: file_id)
-        end
-      end
-
-    child_works_metadata.any?(&:nil?) ? false : true
-  rescue StandardError
-    false
   end
 end
