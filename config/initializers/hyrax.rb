f7d4665ad9fba2e980deca1164dfdf534c1c1053
alex <alexanderleeboyd@gmail.com>
2026-09-09 15:17:24 -0400

    remove process for characterizing large files, adjust for lower max file size limit

--- config/initializers/hyrax.rb
+++ config/initializers/hyrax.rb
@@ -3,48 +3,6 @@
 require 'hyrax/file_set_derivatives_service'
 require 'carrierwave'
 
-class LargeFileCharacterizationService
-  DEFAULT_THRESHOLD_BYTES = 10.gigabytes
-
-  MIME_TYPES_BY_EXTENSION = {
-    '.mov' => ['video/quicktime', 'QuickTime video'],
-    '.mp4' => ['video/mp4', 'MPEG-4 video'],
-    '.m4v' => ['video/x-m4v', 'MPEG-4 video'],
-    '.avi' => ['video/x-msvideo', 'AVI video'],
-    '.mkv' => ['video/x-matroska', 'Matroska video'],
-    '.webm' => ['video/webm', 'WebM video'],
-    '.mp3' => ['audio/mpeg', 'MP3 audio'],
-    '.m4a' => ['audio/mp4', 'MPEG-4 audio'],
-    '.wav' => ['audio/wav', 'WAVE audio'],
-    '.flac' => ['audio/flac', 'FLAC audio'],
-    '.pdf' => ['application/pdf', 'PDF']
-  }.freeze
-
-  class << self
-    def run(metadata:, file:, user: ::User.system_user, **options)
-      file_size = Array(metadata.recorded_size).first.presence&.to_i
-      file_size ||= file.size.to_i if file.respond_to?(:size)
-      inferred_type = MIME_TYPES_BY_EXTENSION[File.extname(metadata.original_filename.to_s).downcase]
-
-      unless file_size && file_size > threshold_bytes && inferred_type
-        return Hyrax::Characterization::ValkyrieCharacterizationService.run(metadata: metadata, file: file, user: user, **options)
-      end
-
-      metadata.mime_type = inferred_type.first
-      metadata.format_label = [inferred_type.last]
-      metadata.recorded_size = [file_size.to_s]
-
-      saved = Hyrax.persister.save(resource: metadata)
-      Hyrax.publisher.publish('file.metadata.updated', metadata: saved, user: user)
-      Hyrax.publisher.publish('file.characterized', file_set: Hyrax.query_service.find_by(id: saved.file_set_id), file_id: saved.id.to_s, path_hint: saved.file_identifier.to_s)
-    end
-
-    def threshold_bytes
-      ENV.fetch('LARGE_FILE_CHARACTERIZATION_THRESHOLD_BYTES', DEFAULT_THRESHOLD_BYTES).to_i
-    end
-  end
-end
-
 hyrax_upload_path = Pathname.new(
   ENV.fetch('HYRAX_UPLOAD_PATH', Rails.root.join('tmp', 'uploads').to_s)
 )
@@ -70,7 +28,6 @@ Hyrax.config do |config|
   config.characterization_options = {
     ch12n_tool: ENV.fetch('CH12N_TOOL', 'fits').to_sym
   }
-  config.characterization_service = LargeFileCharacterizationService
 
   config.disable_include_metadata = false
   config.file_set_include_metadata = true
@@ -112,7 +69,7 @@ Hyrax.config do |config|
   config.uploader = {
     limitConcurrentUploads: 20,
     maxNumberOfFiles: 500,
-    maxFileSize: 100.gigabytes
+    maxFileSize: 16.gigabytes
   }
   # Date you wish to start collecting Google Analytic statistics for
   # Leaving it blank will set the start date to when ever the file was uploaded by
@@ -325,6 +282,9 @@ Hyrax.config do |config|
   # config.admin_set_model = "AdminSetResource"
   config.admin_set_model = 'Hyrax::AdministrativeSet'
   config.file_set_model = 'Hyrax::FileSet'
+  Rails.application.config.to_prepare do
+    config.file_set_indexer = FileSetIndexer
+  end
   # Identify the model class name that will be used for Admin Sets in your app
   # (i.e. AdminSet for ActiveFedora, Hyrax::AdministrativeSet for Valkyrie)
   # config.admin_set_model = "AdminSet"
@@ -409,7 +369,6 @@ Rails.application.reloader.to_prepare do
     Hyrax::CustomQueries::FindCountBy,
     Hyrax::CustomQueries::FindByDateRange,
     Hyrax::CustomQueries::FindByModelAndPropertyValue,
-    Hyrax::CustomQueries::FindByOcrTextAndParentDocumentId,
     Hyrax::CustomQueries::FindByPropertyValue
 
   ]
