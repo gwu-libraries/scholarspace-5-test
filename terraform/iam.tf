fe8dd4042c0a486d70847e0cfb930a103cf78729
alex <alexanderleeboyd@gmail.com>
2026-09-10 11:03:24 -0400

    update permissions for derivative cache s3

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -92,6 +92,25 @@ data "aws_iam_policy_document" "ecs_task_policy" {
     resources = ["${aws_s3_bucket.app_bucket.arn}/*"]
   }
 
+  statement {
+    actions = [
+      "s3:ListBucket",
+      "s3:GetBucketLocation",
+    ]
+    resources = [aws_s3_bucket.derivative_cache_bucket.arn]
+  }
+
+  statement {
+    actions = [
+      "s3:GetObject",
+      "s3:PutObject",
+      "s3:DeleteObject",
+      "s3:AbortMultipartUpload",
+      "s3:ListMultipartUploadParts",
+    ]
+    resources = ["${aws_s3_bucket.derivative_cache_bucket.arn}/*"]
+  }
+
   # Required for ECS Exec (rails console via aws ecs execute-command)
   statement {
     actions = [
